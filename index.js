8073d465a28c239666ea06862ee8b6f443842766
akhila chithanoori <cakhila982@gmail.com>
2023-03-02 14:29:30 +0000

    changed month value

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const addDays = require("date-fns/addDays");
 const format = require("date-fns/format");
 function date(days) {
-  const date = new Date("2020-07-22");
+  const date = new Date("2020-08-22");
   const dateAfter = addDays(date, days);
   const day = dateAfter.getDate();
   const month = dateAfter.getMonth();
